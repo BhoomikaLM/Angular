7c17b6fdaad118580d34996af3cc84e93cb587b5
Bhoomika M <bhoomika.m@happiestminds.com>
2022-11-21 16:17:44 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewAppComponent } from './view-app/view-app.component';
+import { PlaceAppComponent } from './place-app/place-app.component';
+import { QueryComponent } from './query/query.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HealthCareComponent } from './health-care/health-care.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+
+
+const appRoutes: Routes=[
+  { path: 'health-care', component:HealthCareComponent},
+  { path: 'welcome', component:WelcomeComponent},
+  { path: 'view-app', component:ViewAppComponent},
+  { path: 'place-app', component:PlaceAppComponent},
+  { path: 'query', component:QueryComponent},
+  { path: 'feedback', component:FeedbackComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewAppComponent,
+    PlaceAppComponent,
+    QueryComponent,
+    WelcomeComponent,
+    HealthCareComponent,
+    FeedbackComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/place-app/place-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-app',
+  templateUrl: './place-app.component.html',
+  styleUrls: ['./place-app.component.css']
+})
+export class PlaceAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
